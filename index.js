d3a85d39d5889cf2211cd9bab492d7e97ca90341
Madamori Joyce <joycemadamori@gmail.com>
2022-09-12 16:38:58 +0100

    First personal commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Challenge 1: Your Age in Days
+document.getElementById("btnClick").addEventListener("click", ageInDays);
+function ageInDays() {
+var birthYear = prompt('What year were you born... Good friend?');
+var yourAgeInDays = (2022- birthYear) * 365;
+var h1 = document.createElement('h1');  
+var textAnswer = document.createTextNode('You are ' + yourAgeInDays +' days old.');
+h1.setAttribute('id', 'ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+document.getElementById("ageInDays").remove();
+//ageInDays.addEventListener("click", reset);
+}
